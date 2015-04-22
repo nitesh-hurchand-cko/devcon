a18b31013195bfc5c3c144061ac3c49d72d95ed5
nitesh-hurchand-checkout <nitesh.hurchand@checkout.com>
2015-04-22 14:42:06 +0400

    .

--- Checkout.DevCon/Checkout.DevConf/Controllers/AccountController.cs
+++ Checkout.DevCon/Checkout.DevConf/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Linq;
+using System.Web;
 using System.Web.Http;
 using Checkout.DevCon.Models;
 using Checkout.DevCon.Validators;
+using Newtonsoft.Json;
 
 namespace Checkout.DevCon.Controllers
 {
@@ -14,6 +16,42 @@ namespace Checkout.DevCon.Controllers
         public AccountController(ILoginModelValidator loginModelValidator)
         {
             _loginModelValidator = loginModelValidator;
+        }
+
+        [Route("jsonp/login/")]
+        public object Get()
+        {
+            if (Request.RequestUri == null)
+                return "Required fields missing.";
+
+            var decodedUrl = HttpUtility.UrlDecode(Request.RequestUri.Query);
+            if (decodedUrl == null)
+                return "Required fields missing.";
+
+            var paramaters = decodedUrl.Split('&');
+            if (paramaters.Length <= 1 || !paramaters[1].StartsWith("jsonp="))
+                 return "Required fields missing.";
+
+            var loginModel = JsonConvert.DeserializeObject<LoginModel>(paramaters[1].Split('=')[1]);
+
+            if (loginModel == null)
+                return "Required fields missing.";
+
+            var validationResults = _loginModelValidator.Validate(loginModel);
+
+            if (!validationResults.IsValid)
+                return validationResults.Errors.Select(x => x.ErrorMessage);
+
+            if (string.IsNullOrWhiteSpace(loginModel.Username)
+                || string.IsNullOrWhiteSpace(loginModel.Password))
+                return new { valid = false, token = "", name = "" };
+
+            if (loginModel.Username.Equals("admin", StringComparison.InvariantCultureIgnoreCase)
+                && loginModel.Password == "admin")
+            {
+                return new { valid = true, token = Guid.NewGuid().ToString(), name = loginModel.Username };
+            }
+            return new { valid = false, token = "", name = "" };
         }
 
         [Route("login")]
